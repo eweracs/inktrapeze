89ae7f1150a18a429c3edefa75908bb35e4756c8
Sebastian Carewe <sebastian.carewe@gmail.com>
2022-08-22 01:10:41 +0100

    Sratch 90 degree limitation, add TODOs

--- Inktrapeze.glyphsFilter/Contents/Resources/plugin.py
+++ Inktrapeze.glyphsFilter/Contents/Resources/plugin.py
@@ -17,6 +17,8 @@
 # TODO: preview
 # TODO: curved or straight inktrap
 # TODO: fix crash when prevNode or nextNode is first node
+# TODO: Calculate area inside inktrap, base length of trap on that area (using distance between intersections,
+#  and the fact that the trap is symmetrical)
 
 from __future__ import division, print_function, unicode_literals
 import objc
@@ -27,8 +29,6 @@ from Foundation import NSPoint
 
 
 class Inktrapeze(FilterWithDialog):
-
-	# Definitions of IBOutlets
 
 	# The NSView object from the User Interface. Keep this here!
 	dialog = objc.IBOutlet()
@@ -45,8 +45,8 @@ class Inktrapeze(FilterWithDialog):
 			"de": "Inktrapeze",
 			"fr": "Inktrapeze",
 			"es": "Inktrapeze",
-			})
-		
+		})
+
 		# Word on Run Button (default: Apply)
 		self.actionButtonLabel = Glyphs.localize({
 			"en": "Apply",
@@ -57,8 +57,8 @@ class Inktrapeze(FilterWithDialog):
 			"jp": "申し込む",
 			"ko": "대다",
 			"zh": "应用",
-			})
-		
+		})
+
 		# Load dialog from .nib (without .extension)
 		self.loadNib("IBdialog", __file__)
 
@@ -136,75 +136,72 @@ class Inktrapeze(FilterWithDialog):
 		angle_at_node = degrees(acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))
 
 		# see how far into my angle the circle can be pushed, then calculate the intersections with b and c
-		# if the angle is less than 90 degrees, the circle can be pushed inwards
-		if angle_at_node < 90:
-			# calculate the size of the other two equal angles in the triangle consisting of the intersection points of the
-			# pushed-in circle and the selected node
-			other_angles = (180 - angle_at_node) / 2
-			# make a right triangle using the diameter of the circle as the hypotenuse. The right angle will be at the
-			# intersection of the circle with line c.
-			# calculate the angle on in the triangle at the intersection of the circle with line c
-			circle_angle = 90 - other_angles
-			# calculate the distance from the intersection of the circle with line c to the hypotenuse
-			circle_line_b = sin(circle_angle)
-			# calculate the distance from the intersection of the circle with line c to the intersection with line b
-			line_between_intersections = sqrt(aperture ** 2 - circle_line_b ** 2)
+		# calculate the size of the other two equal angles in the triangle consisting of the intersection points of the
+		# pushed-in circle and the selected node
+		other_angles = (180 - angle_at_node) / 2
+		# make a right triangle using the diameter of the circle as the hypotenuse. The right angle will be at the
+		# intersection of the circle with line c.
+		# calculate the angle on in the triangle at the intersection of the circle with line c
+		circle_angle = 90 - other_angles
+		# calculate the distance from the intersection of the circle with line c to the hypotenuse
+		circle_line_b = sin(circle_angle)
+		# calculate the distance from the intersection of the circle with line c to the intersection with line b
+		line_between_intersections = sqrt(aperture ** 2 - circle_line_b ** 2)
 
-			# using the angle A and the two other angles, as well as line_a as the line opposite angle A, calculate the
-			# distance of the intersections to the selected node
-			distance_to_intersections = line_between_intersections * sin(radians(other_angles)) / sin(
-				radians(angle_at_node))
+		# using the angle A and the two other angles, as well as line_a as the line opposite angle A, calculate the
+		# distance of the intersections to the selected node
+		distance_to_intersections = line_between_intersections * sin(radians(other_angles)) / sin(
+			radians(angle_at_node))
 
-			# calculate the path time at which the intersection with line b is reached
-			factor_b = distance_to_intersections / b
-			# calculate the coordinates of intersection b using the coordinates of the selected node and of the left node
-			intersection_b = NSPoint(node.position.x + (prev_node.position.x - node.position.x) * factor_b,
-			                         node.position.y + (prev_node.position.y - node.position.y) * factor_b)
-			# calculate the path time at which the intersection with line c is reached
-			factor_c = distance_to_intersections / c
-			# calculate the coordinates of intersection c using the coordinates of the selected node and of the right node
-			intersection_c = NSPoint(node.position.x + (next_node.position.x - node.position.x) * factor_c,
-			                         node.position.y + (next_node.position.y - node.position.y) * factor_c)
+		# calculate the path time at which the intersection with line b is reached
+		factor_b = distance_to_intersections / b
+		# calculate the coordinates of intersection b using the coordinates of the selected node and of the left node
+		intersection_b = NSPoint(node.position.x + (prev_node.position.x - node.position.x) * factor_b,
+		                         node.position.y + (prev_node.position.y - node.position.y) * factor_b)
+		# calculate the path time at which the intersection with line c is reached
+		factor_c = distance_to_intersections / c
+		# calculate the coordinates of intersection c using the coordinates of the selected node and of the right node
+		intersection_c = NSPoint(node.position.x + (next_node.position.x - node.position.x) * factor_c,
+		                         node.position.y + (next_node.position.y - node.position.y) * factor_c)
 
-			path.insertNode_atIndex_(GSNode(intersection_b), node.index)
-			path.insertNode_atIndex_(GSNode(intersection_c), node.index + 1)
+		path.insertNode_atIndex_(GSNode(intersection_b), node.index)
+		path.insertNode_atIndex_(GSNode(intersection_c), node.index + 1)
 
-			# open a corner at the node
-			layer.openCornerAtNode_offset_(node, distance_to_intersections * depth)
-			# find the middle between the two new nodes
-			node_1_position = path.nodes[node.index].position
-			node_2_position = path.nodes[node.index - 1].position
-			middle_node = GSNode()
-			middle_node.position = NSPoint(node_1_position.x + (node_2_position.x - node_1_position.x) / 2,
-			                               node_1_position.y + (node_2_position.y - node_1_position.y) / 2)
-			path.insertNode_atIndex_(middle_node, node.index)
-			path.removeNodeAtIndex_(node.index - 2)
-			path.removeNodeAtIndex_(node.index)
+		# open a corner at the node
+		layer.openCornerAtNode_offset_(node, distance_to_intersections * depth)
+		# find the middle between the two new nodes
+		node_1_position = path.nodes[node.index].position
+		node_2_position = path.nodes[node.index - 1].position
+		middle_node = GSNode()
+		middle_node.position = NSPoint(node_1_position.x + (node_2_position.x - node_1_position.x) / 2,
+		                               node_1_position.y + (node_2_position.y - node_1_position.y) / 2)
+		path.insertNode_atIndex_(middle_node, node.index)
+		path.removeNodeAtIndex_(node.index - 2)
+		path.removeNodeAtIndex_(node.index)
 
-			# if curved:
-			# 	offcurve_1 = GSNode(self.lerp(0.33,
-			# 	                              path.nodes[middle_node.index - 1].position,
-			# 	                              middle_node.position),
-			# 	                    OFFCURVE)
-			# 	offcurve_2 = GSNode(self.lerp(0.66,
-			# 	                              path.nodes[middle_node.index - 1].position,
-			# 	                              middle_node.position),
-			# 	                    OFFCURVE)
-			# 	path.nodes.insert(middle_node.index, offcurve_1)
-			# 	path.nodes.insert(middle_node.index, offcurve_2)
-			# 	middle_node.type = CURVE
-			#
-			# 	offcurve_3 = GSNode(self.lerp(0.33,
-			# 	                              middle_node.position,
-			# 	                              path.nodes[middle_node.index - 1].position),
-			# 	                    OFFCURVE)
-			# 	offcurve_4 = GSNode(self.lerp(0.66,
-			# 	                              middle_node.position,
-			# 	                              path.nodes[middle_node.index - 1].position),
-			# 	                    OFFCURVE)
-			# 	path.nodes.insert(middle_node.index, offcurve_3)
-			# 	path.nodes.insert(middle_node.index, offcurve_4)
-
+	# if curved:
+	# 	offcurve_1 = GSNode(self.lerp(0.33,
+	# 	                              path.nodes[middle_node.index - 1].position,
+	# 	                              middle_node.position),
+	# 	                    OFFCURVE)
+	# 	offcurve_2 = GSNode(self.lerp(0.66,
+	# 	                              path.nodes[middle_node.index - 1].position,
+	# 	                              middle_node.position),
+	# 	                    OFFCURVE)
+	# 	path.nodes.insert(middle_node.index, offcurve_1)
+	# 	path.nodes.insert(middle_node.index, offcurve_2)
+	# 	middle_node.type = CURVE
+	#
+	# 	offcurve_3 = GSNode(self.lerp(0.33,
+	# 	                              middle_node.position,
+	# 	                              path.nodes[middle_node.index - 1].position),
+	# 	                    OFFCURVE)
+	# 	offcurve_4 = GSNode(self.lerp(0.66,
+	# 	                              middle_node.position,
+	# 	                              path.nodes[middle_node.index - 1].position),
+	# 	                    OFFCURVE)
+	# 	path.nodes.insert(middle_node.index, offcurve_3)
+	# 	path.nodes.insert(middle_node.index, offcurve_4)
 
 	@objc.python_method
 	def __file__(self):
